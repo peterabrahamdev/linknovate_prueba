d69f24e1b0e5b2d5304cd1f09b004ae0e9ceac07
Peter Abraham <peterabrahamdev@gmail.com>
2024-06-05 18:16:05 +0200

    chore: convert voronoi diagram to ConsumerStatefulWidget

--- lib/widgets/voronoi_diagram.dart
+++ lib/widgets/voronoi_diagram.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class VoronoiDiagram extends StatelessWidget {
+class VoronoiDiagram extends ConsumerStatefulWidget {
   const VoronoiDiagram({super.key});
 
+  @override
+  ConsumerState<VoronoiDiagram> createState() => _VoronoiDiagramState();
+}
+
+class _VoronoiDiagramState extends ConsumerState<VoronoiDiagram> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
