6e7af2606a20bd045e24994731683ebc2aacbd6b
Peter Abraham <peterabrahamdev@gmail.com>
2024-06-05 17:59:33 +0200

    feat: add data analytics screen

---
+++ lib/screens/data_analytics_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class DataAnalyticsScren extends StatefulWidget {
+  const DataAnalyticsScren({super.key});
+
+  @override
+  State<DataAnalyticsScren> createState() => _DataAnalyticsScrenState();
+}
+
+class _DataAnalyticsScrenState extends State<DataAnalyticsScren> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          Container(
+            color: Colors.white,
+            
+          )
+        ],
+      ),
+    );
+  }
+}
