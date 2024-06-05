e961e7f19ec97497f7069b2fd1525108724f2e40
Peter Abraham <peterabrahamdev@gmail.com>
2024-06-05 18:04:42 +0200

    feat: create voronoi diagram screen

---
+++ lib/widgets/voronoi_diagram.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VoronoiDiagram extends StatelessWidget {
+  const VoronoiDiagram({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
